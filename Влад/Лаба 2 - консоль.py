b6600ca55e0d3ab94b29356a800d3c1e3d3ac65c
luvidmi <welles7777@gmail.com>
2024-11-09 21:53:51 +0700

    Добавил изменения в лабораторной работе (+параметр покраски авто)

--- Влад/Лаба 2 - консоль.py
+++ Влад/Лаба 2 - консоль.py
@@ -3,13 +3,14 @@
 from prettytable import PrettyTable
 
 class Car():
-    def __init__(self, number, brand, model, engine_type, color, power):
+    def __init__(self, number, brand, model, engine_type, color, power, colored):
         self.number = number
         self.brand = brand
         self.model = model
         self.engine_type = engine_type
         self.color = color
         self.power = power
+        self.colored = colored
     def change(self):
         parameter = input("Какой параметр вы хотите изменить?\n")
         match parameter.split():
@@ -51,6 +52,10 @@ class Car():
                 inp = input('Введите новое значение мощности двигателя автомобиля: ')
                 self.power = inp
                 S[first+5] = self.power
+            case ["Крашена?"]:
+                inp = input('Введите параметр покраски авто (да/нет): ')
+                self.colored = inp
+                S[first+6] = self.colored
                 
         for i in range(0,len(S)):
             S[i]=S[i]+' '
@@ -63,12 +68,14 @@ class Car():
         self.engine_type = ''
         self.color = ''
         self.power = ''
+        self.colored = ''
         S[first] = self.number
         S[first + 1]= self.brand
         S[first + 2]=self.model
         S[first + 3]=self.engine_type
         S[first + 4]=self.color
         S[first + 5]=self.power
+        S[first + 6]=self.colored
         for i in range(0,len(S)):
             S[i]=S[i]+' '
         with open('cars_table.txt', 'w') as f:
@@ -78,24 +85,24 @@ while True:
     option = input("Выберите нужную опцию:\n\n\t1. Вывести таблицу\n\t2. Добавить автомобиль\n\t3. Изменить автомобить\n\t4. Удалить автомобиль\n\t5. Найти автомобиль\n\t6. Выйти из программы\n")
     match option.split():
         case ["1"]:#вывести таблицу
-            table = PrettyTable(["Номер", "Марка", "Модель", "Тип двигателя", "Цвет", "Мощность"])
+            table = PrettyTable(["Номер", "Марка", "Модель", "Тип двигателя", "Цвет", "Мощность", "Крашена?"])
             with open("cars_table.txt", "r") as t:
                 S = t.read().split()
-            length = (len(S) + 1) / 6
+            length = (len(S) + 1) / 7
             first = 0
-            last = 6
+            last = 7
             for i in range(0, int(length)):
                 table.add_row(S[first:last])
-                first += 6
-                last += 6
+                first += 7
+                last += 7
             print(table)
         case ["2"]:#добавить авто
             with open ("cars_table.txt", "r") as t:
                 S = t.read().split()
-            length = (len(S) + 1) / 6
+            length = (len(S) + 1) / 7
             num = input("Введите регистрационный номер добавляемого автомобиля: ")
             while True:
-                for i in range(0, int(length)*6, 6):
+                for i in range(0, int(length)*7, 7):
                     if num == S[i]:
                         num = input("Данный регистрационный номер уже находится в базе данных.\nПожалуйста, введите другой номер: ") 
                 else:
@@ -115,7 +122,8 @@ while True:
                 case ["3"]: engine_type = "Гибридный"
             color = input('Введите новый цвет автомобиля: ')
             power = input('Введите новое значение мощности двигателя автомобиля: ')
-            S_add = [num,' ', brand,' ', model,' ', engine_type,' ', color,' ', power,' ']
+            colored = input('Введите параметр покраски авто (да/нет): ')
+            S_add = [num,' ', brand,' ', model,' ', engine_type,' ', color,' ', power,' ', colored,' ']
             with open ("cars_table.txt", "a") as t:
                 t.writelines(S_add)
             print("\nАвтомобиль успешно добавлен в базу данных.\n")
@@ -128,21 +136,21 @@ while True:
                     break
                 else:
                     num = input("Автомобиль с таким номером отсутствует в базе данных.\nПожалуйста, введите другой номер: ")        
-            length = (len(S) + 1) / 6
+            length = (len(S) + 1) / 7
             first = 0
             for i in range(1, int(length)):
-                first += 6
-            car = Car(S[first], S[first + 1], S[first + 2], S[first + 3], S[first + 4], S[first + 5])
+                first += 7
+            car = Car(S[first], S[first + 1], S[first + 2], S[first + 3], S[first + 4], S[first + 5], S[first + 6])
             car.change()
             print("\nДанные автомобиля успешно изменены.\n")
         case ["4"]:#удаление авто
             with open ("cars_table.txt", "r") as t:
                 S = t.read().split()   
-            length = (len(S) + 1) / 6
+            length = (len(S) + 1) / 7
             first = 0
             num = input("Введите регистрационный номер нужного автомобиля: ")
             while True:
-                for i in range(0, int(length)*6, 6):
+                for i in range(0, int(length)*7, 7):
                     if num == S[i]:
                         first = i
                         break
@@ -150,18 +158,18 @@ while True:
                     break
                 else:
                     num = input("Автомобиль с таким номером отсутствует в базе данных.\nПожалуйста, введите другой номер: ")
-            car = Car(S[first], S[first + 1], S[first + 2], S[first + 3], S[first + 4], S[first + 5])
+            car = Car(S[first], S[first + 1], S[first + 2], S[first + 3], S[first + 4], S[first + 5], S[first + 6])
             car.delete()
             print(f"\nАвтомобиль с номером '{num}' удалён из базы данных.\n")
         case ["5"]:#поиск авто
             with open ("cars_table.txt", "r") as t:
                 S = t.read().split() 
-            tablesearch = PrettyTable(["Номер", "Марка", "Модель", "Тип двигателя", "Цвет", "Мощность"])
+            tablesearch = PrettyTable(["Номер", "Марка", "Модель", "Тип двигателя", "Цвет", "Мощность", "Крашена?"])
             searchpar = input("Введите параметр, по которому хотите произвести поиск: ")
-            length = (len(S) + 1) / 6
-            for i in range(0, int(length)*6, 6):
-                if searchpar in S[i:i+6]:
-                    tablesearch.add_row(S[i:i+6])
+            length = (len(S) + 1) / 7
+            for i in range(0, int(length)*7, 7):
+                if searchpar in S[i:i+7]:
+                    tablesearch.add_row(S[i:i+7])
                     print("По вашему запросу найдены совпадения: \n", tablesearch)
                 else:
                     print("По вашему запросу ничего не найдено. \n")
